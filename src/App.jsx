a196c80b0f08170bf0feb5f1562864ab92afd65c
JDomingoSilva <jdsilvam@uc.cl>
2022-04-14 17:53:09 -0400

    feat: tried a navbar mimic, also changed routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import RouteList from './routes';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <RouteList />
+    <Router>
+      <Navbar />
+      <RouteList />
+    </Router>
   );
 }
 
