43a7eb9665384722fdb41f1b20f246b2b5f73f0b
JDomingoSilva <jdsilvam@uc.cl>
2022-04-16 16:20:58 -0400

    xd

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Calendar from './views/Calendar';
 
 export default function RouteList() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/calendar" element={<Calendar />} />
     </Routes>
   );
 }
