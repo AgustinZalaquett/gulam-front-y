c1185b007c5afde98a03d0180e41e673032d50db
AgustinZalaquett <jagustin.zalaquett@uc.cl>
2022-04-16 19:14:22 -0400

    feat: agregar flujo básico

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <div>
       <h1>Hola soy una navbar</h1>
+      <div className="navbar-buttons-container">
+        <Link to="/login" className="login">Log in</Link>
+        <Link to="/signup" className="login">Sign Up</Link>
+        <Link to="/" className="login">Main Menu</Link>
+      </div>
     </div>
   );
 }
